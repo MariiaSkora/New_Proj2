a5155ac459fd6ec31852dbe02ea3f65b51ef47de
Mariia Skora <mariia/skora@globallogic.com>
2018-11-08 14:52:43 +0200

    commit 16

--- src/class2.java
+++ src/class2.java
@@ -1,7 +1,15 @@
 public class class2 {
     public static void main(String[] args) {
         System.out.println("hello");
+<<<<<<< Updated upstream
         //commit
+=======
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
+        //commit verified
+>>>>>>> Stashed changes
     }
 }
 
